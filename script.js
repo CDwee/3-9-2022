4960db6f24905ebcbe06afe179ee11e54928ce85
CDwee <96454637+CDwee@users.noreply.github.com>
2022-03-09 19:03:49 -0600

    Update and rename README.md to script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+// Started at 5:54 3-9-2022
+
+ <script type="module" src="/main.js"></script>
+ <script src='https://cdn.rawgit.com/sarcadass/granim.js/master/dist/granim.min.js'></script>
+
+ import * as THREE from 'https://cdn.skypack.dev/three@0.132.2';
+
+import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.132.2/examples/jsm/loaders/GLTFLoader.js';
+import { DRACOLoader } from 'https://cdn.skypack.dev/three@0.132.2/examples/jsm/loaders/DRACOLoader.js';
+
+// Ended at 7:02 3-9-2022
